4cd7b6dc1a3e99c871c35d181f12d4a9d1e69b34
bala-amypo <bala@amypo.in>
2025-12-18 08:27:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,6 +2,12 @@ package com.example.demo.controller;
 @RestController
 public class AuthController{
     @PostMapping("/register")
-    public User createData(@RequestBody )
+    public User createData(@RequestBody User use){
+        return 
+    }
+    @PostMapping("/login")
+    public User createdData(@RequestBody User use){
+        return 
+    }
 
 }
