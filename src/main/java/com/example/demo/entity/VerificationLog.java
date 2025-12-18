5e7163119b0e299efabc361fd92a0c3a4b10cca9
bala-amypo <bala@amypo.in>
2025-12-18 05:48:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 @Entity
 @Builder
 public class VerificationLog{
@@ -13,5 +14,62 @@ public class VerificationLog{
     private LocalDateTime verifiedAt;
     private String status;
     private String ipAddress;
+     
+    public VerificationLog() {
+    }
+
+    public VerificationLog(Certificate certificate, String status, String ipAddress) {
+        this.certificate = certificate;
+        this.status = status;
+        this.ipAddress = ipAddress;
+    }
+
+    
+    @PrePersist
+    private void onVerify() {
+        this.verifiedAt = LocalDateTime.now();
+
+      
+        if (status != null) {
+            this.status = status.toUpperCase();
+        }
+    }
+
+   
+    public long getId() {
+        return id;
+    }
+
+    public Certificate getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(Certificate certificate) {
+        this.certificate = certificate;
+    }
+
+    public LocalDateTime getVerifiedAt() {
+        return verifiedAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+  
+    public void setStatus(String status) {
+        if (!"SUCCESS".equalsIgnoreCase(status) && !"FAILED".equalsIgnoreCase(status)) {
+            throw new IllegalArgumentException("Status must be SUCCESS or FAILED");
+        }
+        this.status = status.toUpperCase();
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 
 }
