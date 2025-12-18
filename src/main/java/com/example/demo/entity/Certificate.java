43418340d74ce1eaaf7535b468fc52ee8d7a4f18
bala-amypo <bala@amypo.in>
2025-12-18 05:13:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -4,13 +4,79 @@ import jakarta.persistence.*;
 @Entity
 @Builder
 public class Certificate{
-     private int id;
+    @id
+     private long id;
      @ManyToOne
-     
-    @JoinColumn(name = "student_id", nullable = false)
-    private Visitor visitor;
+     @JoinColumn(name = "student_id", nullable = false)
+     private Student student;
+      @ManyToOne
+     @JoinColumn(name = "template_id", nullable = false)
+     private CertificateTemplate template;
+    private LocalDate issuedDate;
      private String qrcodeUrl;
      @Column(unique=true)
      private String verificationCode;
+     public Certificate() {
+    }
 
+   
+    public Certificate(long id, Student student, CertificateTemplate template,
+                       LocalDate issuedDate, String qrcodeUrl, String verificationCode) {
+        this.id = id;
+        this.student = student;
+        this.template = template;
+        this.issuedDate = issuedDate;
+        this.qrcodeUrl = qrcodeUrl;
+        this.verificationCode = verificationCode;
+    }
+
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public CertificateTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(CertificateTemplate template) {
+        this.template = template;
+    }
+
+    public LocalDate getIssuedDate() {
+        return issuedDate;
+    }
+
+    public void setIssuedDate(LocalDate issuedDate) {
+        this.issuedDate = issuedDate;
+    }
+
+    public String getQrcodeUrl() {
+        return qrcodeUrl;
+    }
+
+    public void setQrcodeUrl(String qrcodeUrl) {
+        this.qrcodeUrl = qrcodeUrl;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
 }
+
