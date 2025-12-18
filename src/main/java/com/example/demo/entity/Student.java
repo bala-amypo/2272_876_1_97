6916f66ad27640cd0efdae9ef807cac46afe07e6
bala-amypo <bala@amypo.in>
2025-12-18 10:02:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,62 +1,27 @@
 package com.example.demo.entity;
-import lombok.Builder;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public class Student{
-    
+public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
-    @Column(unique=true)
+
+    @Column(unique = true, nullable = false)
     private String email;
-    @Column(unique=true)
+
+    @Column(unique = true, nullable = false)
     private String rollNumber;
-       
-    public Student() {
-    }
-
-   
-    public Student(long id, String name, String email, String rollNumber) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.rollNumber = rollNumber;
-    }
-
-    
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRollNumber() {
-        return rollNumber;
-    }
-
-    public void setRollNumber(String rollNumber) {
-        this.rollNumber = rollNumber;
-    }
-
 }
---
+++ src/main/java/com/example/demo/service/implementation/StudentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.service.implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User createData(User user) {
+        return repository.save(user); 
+    }
+
+    public User logData(User user) {
+        return repository.save(user);
+    }
+}
