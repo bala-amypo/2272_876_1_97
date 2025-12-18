663fd906dfac53b79a4b1a73a8511a89c08c4079
bala-amypo <bala@amypo.in>
2025-12-18 05:38:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,6 +10,70 @@ public class User{
     private String name;
     @Column(unique=true)
     private String email;
-    private 
+    private  String password;
+    private String role;
+     public User() {
+        this.role = "STAFF";   // default role
+    }
 
+    // ✅ Parameterized constructor
+    public User(long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = (role == null || role.isEmpty()) ? "STAFF" : role;
+    }
+
+    
+    @PrePersist
+    @PreUpdate
+    private void hashPassword() {
+        if (this.password != null && !this.password.startsWith("$2a$")) {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            this.password = encoder.encode(this.password);
+        }
+    }
+
+   
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = (role == null || role.isEmpty()) ? "STAFF" : role;
+    }
 }
