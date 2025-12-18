14564d8498f0f55e61fe0cf3f88b0a14a5c7d326
bala-amypo <bala@amypo.in>
2025-12-18 09:47:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,20 +5,20 @@ import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
 @RestController
-
+@RequestMapping("/students")
 public class StudentController{
     private final StudentService studentservice;
     public StudentController(StudeService studentservice){
-        this.userservice=userservice;
+        this.studentservice=studentservice;
     }
 
     
-    @PostMapping("/register")
-    public User createData(@RequestBody User user){
-        return  userservice.createData(use);
+    @PostMapping("/add")
+    public Student createData(@RequestBody Student stu){
+        return  userservice.createData(stu);
     }
-    @PostMapping("/login")
-    public User createdData(@RequestBody User user){
+    @GetMapping("/addalldata")
+    public Student getData(@RequestBody User user){
         return  userservice.logData(use);
     }
 
