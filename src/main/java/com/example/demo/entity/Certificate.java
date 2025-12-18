11379599a08adc25262b5135c247c04a339bacc7
bala-amypo <bala@amypo.in>
2025-12-18 05:08:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -1,9 +1,16 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 @Entity
 @Builder
 public class Certificate{
      private int id;
+     @ManyToOne
      
+    @JoinColumn(name = "student_id", nullable = false)
+    private Visitor visitor;
+     private String qrcodeUrl;
+     @Column(unique=true)
+     private String verificationCode;
+
 }
