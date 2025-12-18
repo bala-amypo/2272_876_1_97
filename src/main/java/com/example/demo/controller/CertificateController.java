7370491d9f15593d5985d8c6b56ef7340390d370
bala-amypo <bala@amypo.in>
2025-12-18 15:24:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CertificateController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Certificate;
+import com.example.demo.service.CertificateService;
+
+@RestController
+@RequestMapping("/certificates")
+public class CertificateController {
+
+    private final CertificateService certificateService;
+
+    public CertificateController(CertificateService certificateService) {
+        this.certificateService = certificateService;
+    }
+
+   
+    @PostMapping("/generate/{studentId}/{templateId}")
+    public Certificate generateCertificate(
+            @PathVariable Long studentId,
+            @PathVariable Long templateId) {
+
+        return certificateService.generateCertificate(studentId, templateId);
+    }
+
+ 
+    @GetMapping("/{certificateId}")
+    public Certificate getCertificate(
+            @PathVariable Long certificateId) {
+
+        return certificateService.getCertificateById(certificateId);
+    }
+
+    
+    @GetMapping("/verify/code/{verificationCode}")
+    public Certificate getCertificateByCode(
+            @PathVariable String verificationCode) {
+
+        return certificateService.getCertificateByVerificationCode(verificationCode);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/VerificationController.java
@@ -0,0 +1,36 @@
+// package com.example.demo.controller;
+
+// import java.util.List;
+
+// import org.springframework.web.bind.annotation.*;
+
+// import com.example.demo.entity.VerificationLog;
+// import com.example.demo.service.VerificationService;
+
+// @RestController
+// @RequestMapping("/verify")
+// public class VerificationController {
+
+//     private final VerificationService verificationService;
+
+//     public VerificationController(VerificationService verificationService) {
+//         this.verificationService = verificationService;
+//     }
+
+   
+//     @PostMapping("/{verificationCode}")
+//     public String verifyCertificate(
+//             @PathVariable String verificationCode) {
+
+//         verificationService.verifyCertificate(verificationCode);
+//         return "Certificate verified successfully";
+//     }
+
+   
+//     @GetMapping("/logs/{certificateId}")
+//     public List<VerificationLog> getVerificationLogs(
+//             @PathVariable Long certificateId) {
+
+//         return verificationService.getLogsByCertificateId(certificateId);
+//     }
+// }
