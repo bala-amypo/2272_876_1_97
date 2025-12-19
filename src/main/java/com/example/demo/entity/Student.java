ef189a96ab52b9f9a76b718c2a6f2e6ea2110409
bala-amypo <bala@amypo.in>
2025-12-19 04:34:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -3,6 +3,8 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "students")
 @Getter
@@ -24,4 +26,7 @@ public class Student {
 
     @Column(unique = true, nullable = false)
     private String rollNumber;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Certificate> certificates;
 }
