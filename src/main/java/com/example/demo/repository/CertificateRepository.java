7b7ef6c872a7ad1edb3e51369e71c3320ee092cc
bala-amypo <bala@amypo.in>
2025-12-19 10:28:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/CertificateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateRepository.java
@@ -4,11 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Certificate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CertificateRepository
         extends JpaRepository<Certificate, Long> {
 
+    // Used by VerificationService
     Optional<Certificate> findByVerificationCode(String verificationCode);
+
+    // Used by CertificateServiceImpl
+    Optional<Certificate> findByCode(String code);
+
+    List<Certificate> findByStudentId(Long studentId);
 }
