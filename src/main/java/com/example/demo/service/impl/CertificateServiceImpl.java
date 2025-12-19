2945ccaf0f5758e27ba4ef2970d55b44ec4e6ebf
bala-amypo <bala@amypo.in>
2025-12-19 15:34:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CertificateServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CertificateServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entity.Certificate;
+import com.example.demo.entity.Student;
 import com.example.demo.repository.CertificateRepository;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.CertificateService;
 
 import java.util.List;
@@ -11,20 +14,32 @@ import java.util.List;
 public class CertificateServiceImpl implements CertificateService {
 
     private final CertificateRepository certificateRepository;
+    private final StudentRepository studentRepository;
 
-    public CertificateServiceImpl(CertificateRepository certificateRepository) {
+    public CertificateServiceImpl(
+            CertificateRepository certificateRepository,
+            StudentRepository studentRepository) {
         this.certificateRepository = certificateRepository;
+        this.studentRepository = studentRepository;
     }
 
     @Override
     public Certificate addCertificate(Certificate certificate) {
+
+        Long studentId = certificate.getStudent().getId();
+
+        Student managedStudent = studentRepository.findById(studentId)
+                .orElseThrow(() ->
+                        new RuntimeException("Student not found with id: " + studentId));
+
+        certificate.setStudent(managedStudent);
+
         return certificateRepository.save(certificate);
     }
 
     @Override
     public Certificate getCertificateByCode(String code) {
-        return certificateRepository
-                .findByCode(code)
+        return certificateRepository.findByCode(code)
                 .orElseThrow(() ->
                         new RuntimeException("Certificate not found with code: " + code));
     }
